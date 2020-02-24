48ffde0ef5ad8b1deeb3c2ed8485e788fe5b791f
Nils Müller <nils.mueller@strg.at>
2020-02-24 11:02:09 +0100

    New: implemented logging library BHV-696

---
+++ README.md
@@ -0,0 +1,56 @@
+# @strg/logging-winston
+This library provides a preconfigured winston logger that can be used in any typescript or node project
+
+## Getting Started
+
+### Prerequisites
+- [Node v10](https://nodejs.org) or greater, NPM v6.4.1
+<br>
+<br>
+
+### Installing the library
+
+```bash
+npm install @strg/logging-winston
+```
+
+### Using the client library
+```javascript
+import {logger} from '@strg/logging-winston'
+
+logger.info(`${'hello world'}`)
+
+```
+
+
+### ENV
+
+|    | Description | Default |
+|:---|:------------|:--------|
+| **NODE_LOG_FORMAT** | _`SIMPLE`_, _`JSON`_ | `JSON` |
+| **NODE_LOG_LEVEL** | _`fatal`_,  _`error`_, _`warn`_, _`info`_, _`trace`_, _`debug`_ | `info` |
+<br>
+<br>
+
+## Specification
+This library aims to standarize the logging output in all node or typescript based applications.
+<br>
+<br>
+
+## Development
+
+### Testing
+`#TBD`
+
+### Coverage Report
+`#TBD`
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the tags on this repository.
+<br>
+<br>
+
+
+## Authors
+* **[Nils Müller](mailto:nils.mueller@strg.at)** - *implementation*
---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import winston = require('winston')
+import { Format, TransformableInfo } from 'logform'
+ 
+
+const NODE_LOG_FORMAT: string = process.env.NODE_LOG_FORMAT && process.env.NODE_LOG_FORMAT.toLowerCase() === 'simple' ? 'simple' : 'json'
+const NODE_LOG_LEVEL: string = process.env.NODE_LOG_LEVEL ? process.env.NODE_LOG_LEVEL : 'info'
+
+winston.addColors({
+  fatal: 'magenta',
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  trace: 'blue',
+  debug: 'grey',
+})
+
+const displayStack = winston.format((info: TransformableInfo) => {
+  info.message = info.stack ? info.stack : info.message
+  return info
+})
+
+const jsonFormat: Format = winston.format.combine(
+  displayStack(),
+  winston.format.metadata(),
+  winston.format.json()
+)
+
+const simpleFormat: Format = winston.format.combine(
+  displayStack(),
+  winston.format.colorize({ all: true }),
+  winston.format.simple(),
+)
+
+const logger = winston.createLogger({
+  transports: [new winston.transports.Console({
+    stderrLevels: ['fatal', 'error'],
+    level: NODE_LOG_LEVEL,
+  })],
+  format: NODE_LOG_FORMAT === 'simple' ? simpleFormat : jsonFormat,
+  levels: {
+    fatal: 0,
+    error: 1,
+    warn: 2,
+    info: 3,
+    trace: 4,
+    debug: 5,
+  },
+})
+
+// always log uncaught to error log
+process.on('uncaughtException', (e) => logger.error(e))
+
+
+// always log unhandled promise rejection to error log
+process.on('unhandledRejection', (reason: {} | null | undefined, promise: Promise<any>) => {
+  if (reason instanceof Error) {
+    logger.error(reason)
+  } else {
+    logger.error(new Error(`${reason}`))
+  }
+})
+
+export { logger }
