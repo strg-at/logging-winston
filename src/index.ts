2e33e7cd3668d3db9cc0ccaea09381fc031c6a9d
Nils Müller <nils.mueller@strg.at>
2021-02-11 18:27:36 +0100

    Fix: JSON error logging format INPRO-305

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import winston = require('winston')
 import { Format, TransformableInfo } from 'logform'
- 
+
 
 const NODE_LOG_FORMAT: string = process.env.NODE_LOG_FORMAT && process.env.NODE_LOG_FORMAT.toLowerCase() === 'simple' ? 'simple' : 'json'
 const NODE_LOG_LEVEL: string = process.env.NODE_LOG_LEVEL ? process.env.NODE_LOG_LEVEL : 'info'
@@ -14,19 +14,29 @@ winston.addColors({
   debug: 'grey',
 })
 
-const displayStack = winston.format((info: TransformableInfo) => {
+const displayStackSimple = winston.format((info: TransformableInfo) => {
   info.message = info.stack ? info.stack : info.message
   return info
 })
 
+const displayStackJson = winston.format((info: any) => {
+  if (info.level === 'error' && info.message?.error instanceof Error) {
+    return Object.assign({}, info, {
+      stack: info.message.error.stack,
+      message: info.message.error.message
+    })
+  }
+  return info
+});
+
 const jsonFormat: Format = winston.format.combine(
-  displayStack(),
   winston.format.metadata(),
+  displayStackJson(),
   winston.format.json()
 )
 
 const simpleFormat: Format = winston.format.combine(
-  displayStack(),
+  displayStackSimple(),
   winston.format.colorize({ all: true }),
   winston.format.simple(),
 )
